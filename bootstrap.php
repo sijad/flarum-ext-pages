f5421ae1a52803fb82fbeb8c1c5e35d46fbb88ec
Sajjad Hashemian <wolaws@gmail.com>
2018-08-02 14:34:20 +0430

    use new extender

--- bootstrap.php
+++ bootstrap.php
@@ -6,18 +6,19 @@ use Sijad\Pages\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
-    /* (new Extend\Frontend('forum')) */
-    /*     ->js(__DIR__.'/js/dist/forum.js') */
-    /*     ->css(__DIR__.'/less/forum.less'), */
+    new Extend\Locales(__DIR__.'/locale'),
 
-    /* (new Extend\Frontend('admin')) */
-    /*     ->js(__DIR__.'/js/dist/admin.js') */
-    /*     ->css(__DIR__.'/less/admin.less'), */
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
+
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/less/admin.less'),
 
     function (Dispatcher $events) {
         app()->instance('path.pages', base_path().DIRECTORY_SEPARATOR.'pages');
 
-        $events->subscribe(Listener\AddClientAssets::class);
         $events->subscribe(Listener\AddPagesRoute::class);
         $events->subscribe(Listener\AddPagesApi::class);
         $events->subscribe(Listener\AddApiAttributes::class);
